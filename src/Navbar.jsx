fec9b094ec1620342bba1617363341b6fdff4885
jackfr8st <86789457+jackfr8st@users.noreply.github.com>
2024-01-18 13:48:53 +0530

    search bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,12 +1,17 @@
+import { Input } from "postcss";
 import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="p-5 fixed z-10 w-full bg-neutral-900 text-3xl duration-300 hover:text-4xl">
+    <div className="grid grid-cols-3 gap-4 p-5 fixed z-10 w-full bg-neutral-900 text-3xl duration-300 hover:text-4xl">
       <div className="w-fit">
         <Link to="/">Test Site</Link>
       </div>
+      <input
+        placeholder="Search"
+        className="p-5 font-extralight rounded-md text-lg h-6 shadow-2xl"
+      />
     </div>
   );
 };
