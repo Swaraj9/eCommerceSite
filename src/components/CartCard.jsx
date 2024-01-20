6a460deaef88b31e11af5d01ca0e3d92ea7b6cab
jackfr8st <86789457+jackfr8st@users.noreply.github.com>
2024-01-21 00:17:54 +0530

    UI improvements

--- src/components/CartCard.jsx
+++ src/components/CartCard.jsx
@@ -20,22 +20,22 @@ const CartCard = ({ product, amount }) => {
           alt={`${title} thumbnail`}
         />
       </div>
-      <div className="w-full ml-3 flex flex-col justify-between">
+      <div className="w-full mx-3 flex flex-col justify-between">
         <div className="flex justify-between w-full mb-5">
           <div className="flex flex-col">
-            <div className="text-3xl">{title}</div>
+            <div className="text-2xl">{title}</div>
             <div className="text-sm text-neutral-500">{brand}</div>
           </div>
           <div className="flex flex-col text-center">
-            <div className="text-3xl text-green-600">{`${price}$`}</div>
+            <div className="text-2xl text-green-600">{`${price}$`}</div>
             <div className="text-xs  text-orange-500 text-right">{`${discount}% OFF`}</div>
           </div>
         </div>
-        <div className="text-lg flex items-center justify-between">
+        <div className="text-base flex items-center justify-between">
           <div>{`Amount: ${amount}`}</div>
           <div
             onClick={() => dispatch(removeProduct(product))}
-            className="text-red-700 text-2xl border-red-700 border-2 p-2 rounded-md duration-300 hover:text-neutral-900 hover:bg-red-700"
+            className="text-red-700 text-lg border-red-700 border-2 p-2 rounded-md duration-300 hover:text-neutral-900 hover:bg-red-700"
           >
             <FaTrash />
           </div>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -9,47 +9,66 @@ const Cart = () => {
   const productCount = useSelector((state) => state.cart.productCount);
   const totalPrice = useSelector((state) => state.cart.totalPrice);
 
+  let meow = false;
   return (
-    <div className="flex">
+    <div className="flex flex-col">
       <Navbar searchBar={true} />
+      <div className="my-8"></div>
       <div className="flex mt-10 flex-col w-full ">
-        <div className="mt-20 flex flex-col w-full sm:flex-row sm:justify-center">
-          <div className="col-span-3">
+        {meow && (
+          <div className="flex flex-row">
+            <div className="w-1/12"></div>
+            <div className="flex justify-center rounded-lg bg-slate-400 py-6 w-10/12 md:text-lg">
+              Your order was placed.
+            </div>
+            <div className="w-1/12"></div>
+          </div>
+        )}
+        <div className="mt-4 flex flex-col-reverse w-full sm:flex-row sm:justify-center">
+          <div>
             <div className="flex flex-col px-5 py-7 rounded-md">
               <div className="font-normal text-3xl pb-4 ">Shopping cart</div>
               <hr className=" w-full" />
               <div className="p-3">
-                {productCount > 0 ? productObjects.map((productObject) => (
-                  <CartCard
-                    key={productObject.product.id}
-                    product={productObject.product}
-                    amount={productObject.amount}
-                  />
-                )) : <div className="text-neutral-500 text-center text-lg">No Items Added to Cart Yet</div>}
+                {productCount > 0 ? (
+                  productObjects.map((productObject) => (
+                    <CartCard
+                      key={productObject.product.id}
+                      product={productObject.product}
+                      amount={productObject.amount}
+                    />
+                  ))
+                ) : (
+                  <div className="text-neutral-500 text-center text-lg">
+                    No Items Added to Cart Yet
+                  </div>
+                )}
               </div>
               <br />
               <hr className=" w-full py-1" />
               <div className="flex justify-between text-lg">
-                <div>{`Total Items: ${productCount}`}</div>
-                <div>{`Subtotal (items): ${totalPrice}$`}</div>
+                <div></div>
+                <div className="mr-1">{`Subtotal (${productCount}): ${totalPrice}$`}</div>
               </div>
             </div>
           </div>
+          <div className="mx-14"></div>
           <div>
-            <div className="bg-neutral-900 px-5 py-7 grid-rows-1 rounded-md">
+            <div className="bg-neutral-900 px-5 py-7 rounded-md m-auto">
               <div className="flex justify-between text-lg gap-10">
-                <div>{`Subtotal (items): ${totalPrice}$`}</div>
-                <div>{`Total Items: ${productCount}`}</div>
+                <div>{`Subtotal : ${totalPrice}$`}</div>
               </div>
               <div className="text-neutral-400">
                 <Link to="/" reloadDocument>
                   View Products ›
                 </Link>
               </div>
-
-              <button className="w-full text-center text-2xl text-yellow-400 border-yellow-400 border-2 p-1 px-3 rounded-md mt-5 duration-300 hover:bg-yellow-400 hover:text-neutral-900">
-                Proceed to Buy
-              </button>
+              <div className="mt-5">{`Total Items: ${productCount}`}</div>
+              <div className="flex justify-items-center">
+                <button className="m-auto md:mx-4 text-center text-lg md:text-2xl text-yellow-400 border-yellow-400 border-2 p-1 px-5 rounded-md mt-7 duration-300 hover:bg-yellow-400 hover:text-neutral-900">
+                  Proceed to Buy
+                </button>
+              </div>
             </div>
           </div>
         </div>
