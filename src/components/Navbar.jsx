a88802a15c603b2c49b5a35d6110a419c8460c66
Swaraj9 <swaraj.dusane90@gmail.com>
2024-02-01 22:22:11 +0530

    Drawer for mobiles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
-import { FaSearch } from "react-icons/fa";
+import { FaHamburger, FaSearch } from "react-icons/fa";
 import { PiUserCircle } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { useSelector } from "react-redux";
 import { useCategories } from "../hooks/useCategories";
-import { IoClose } from "react-icons/io5";
+import { IoClose, IoMenu } from "react-icons/io5";
 import Switcher from "./Switcher";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 const Category = ({ category, selectedCats, setSelectedCats }) => {
   const [selected, setSelected] = useState(false);
@@ -54,9 +56,11 @@ const Navbar = ({
 
   const categories = useCategories();
 
+  const [drawerOpen, setDrawenOpen] = useState(false);
+
   return (
     <div className="flex flex-col fixed z-10 shadow-2xl dark:shadow-none w-full">
-      <div className="flex p-5 items-center justify-around w-full bg-neutral-200 dark:bg-neutral-800 border-b-2 border-neutral-300 dark:border-neutral-700 text-lg sm:text-3xl duration-300">
+      <div className="flex p-3 sm:p-5 items-center justify-around w-full bg-neutral-200 dark:bg-neutral-800 border-b-2 border-neutral-300 dark:border-neutral-700 text-lg sm:text-3xl duration-300">
         <div className="w-fit text-neutral-500 dark:text-[#FFD60A] duration-100 hover:font-bold mr-5">
           <Link to="/">
             <div>eCommerceSite</div>
@@ -77,7 +81,7 @@ const Navbar = ({
             </button>
           </div>
         )}
-        <div className="text-2xl flex-1 flex items-center gap-3 justify-end ml-5 text-neutral-500 dark:text-neutral-50">
+        <div className="text-2xl sm:flex flex-1 hidden items-center gap-3 justify-end ml-5 text-neutral-500 dark:text-neutral-50">
           <Switcher />
           <Link to="/cart">
             <div className="flex gap-3 dark:border-2 dark:border-neutral-700 rounded-3xl p-3 pt-2 pb-2 justify-center items-center bg-neutral-300 dark:bg-inherit shadow-lg dark:shadow-none">
@@ -91,6 +95,39 @@ const Navbar = ({
             <PiUserCircle />
           </div>
         </div>
+        <button
+          onClick={() => setDrawenOpen(!drawerOpen)}
+          className={`text-neutral-500 ${!searchBar && "ml-auto"} sm:hidden ml-3 text-2xl`}
+        >
+          <IoMenu />
+        </button>
+        <Drawer
+          open={drawerOpen}
+          onClose={() => setDrawenOpen(!drawerOpen)}
+          direction="right"
+          className="flex flex-col text-neutral-400"
+        >
+          <div className="h-full flex p-5 flex-col gap-5 bg-neutral-100 dark:bg-neutral-800">
+            <div className="flex items-end w-full justify-between border-b-2 border-neutral-200 dark:border-neutral-600 pb-2">
+              <PiUserCircle className="text-3xl text-[#e4c428]"/>
+              <div>Username</div>
+            </div>
+            <Link to='/cart'>
+              <div className="flex items-end w-full justify-between border-b-2 border-neutral-200 dark:border-neutral-600 pb-2">
+                <div className="flex flex-row gap-2 items-center text-[#e4c428]">
+                  <MdOutlineShoppingCart className="text-2xl" />
+                  <div>{cartItemCount}</div>
+                </div>
+                <div>Cart</div>
+              </div>
+            </Link>
+            <div className="flex items-end w-full justify-between border-b-2 border-neutral-200 dark:border-neutral-600 pb-2">
+              <Switcher drawer={true} />
+              <div>Theme</div>
+            </div>
+            <button className="bg-[#FFD60A] text-neutral-50 dark:text-neutral-800 rounded-md pt-1 pb-1 mt-auto">Sign Out</button>
+          </div>
+        </Drawer>
       </div>
       {searchBar && (
         <div className="flex pl-2 no-scrollbar overflow-x-scroll  items-center gap-2 pt-3 pb-3 border-b-2 border-neutral-300 dark:border-neutral-700 bg-neutral-200 dark:bg-neutral-800">
--- src/components/Switcher.jsx
+++ src/components/Switcher.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import useDarkMode from "../hooks/useDarkMode";
 import { FaRegMoon, FaRegSun } from "react-icons/fa";
+import { IoSunny } from "react-icons/io5";
 
-const Switcher = () => {
+const Switcher = ({drawer}) => {
   const [colorTheme, setTheme] = useDarkMode();
   const [darkMode, setDarkMode] = useState(
     colorTheme === "light" ? true : false
@@ -16,10 +17,10 @@ const Switcher = () => {
   return (
     <div>
       <button
-        className="dark:border-2 text-base sm:text-2xl dark:border-neutral-700 rounded-3xl p-3 pt-2 pb-2 bg-neutral-300 dark:bg-inherit shadow-lg dark:shadow-none"
+        className={`${!drawer && "dark:border-2"} text-base ${drawer && "text-[#FFD60A] text-xl bg-inherit"} sm:text-2xl dark:border-neutral-700 rounded-3xl ${!drawer && "p-3"} pt-2 pb-2 bg-neutral-300 dark:bg-inherit ${!drawer && "shadow-lg"} dark:shadow-none`}
         onClick={() => toggleDarkMode(darkMode)}
       >
-        {darkMode ? <FaRegMoon className="hover:animate-spin" /> : <FaRegSun className="hover:animate-spin" />}
+        {darkMode ? <FaRegMoon className="hover:animate-spin" /> : <IoSunny className="hover:animate-spin" />}
       </button>
     </div>
   );
